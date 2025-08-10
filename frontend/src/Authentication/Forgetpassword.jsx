864b5f32fe60d114347e46cfdf1f2d0e8c519587
Taha Siraj <Pikachugaming899@gmail.com>
2025-08-10 12:18:25 -0700

    updated

--- frontend/src/Authentication/Forgetpassword.jsx
+++ frontend/src/Authentication/Forgetpassword.jsx
@@ -125,8 +125,8 @@ const Forgetpassword = () => {
                 </div>
             </div>
         )}
-           {step === 4 && (
-          <div className="text-center space-y-6 py-8">
+        {step === 4 && (
+          <div className="text-center space-y-3 py-2">
             <div className="mx-auto w-20 h-20 flex items-center justify-center bg-green-100 rounded-full">
                 <svg className="w-12 h-12 text-green-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path></svg>
             </div>
