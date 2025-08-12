a0e9aab4450d3e43796be0d4974ef23a917ede11
Taha Siraj <Pikachugaming899@gmail.com>
2025-08-12 12:49:11 -0700

    updated

--- frontend/src/Routes/CustomRoutes.jsx
+++ frontend/src/Routes/CustomRoutes.jsx
@@ -9,7 +9,6 @@ import AddProducts from '../admin/AddProducts';
 import AddCategories from '../admin/AddCategories';
 import Productsdetail from '../Pages/Productsdetail';
 import Allblogs from '../Blog/Allblogs';
-import Admindashbord from '../admin/Admindashbord';
 import Cart from '../Pages/Cart';
 import Loader from '../Pages/Loader';
 import Checkout from '../Pages/Checkout';
@@ -19,53 +18,44 @@ const CustomRoutes = () => {
   const { state } = useContext(GlobalContext);
   const isAdmin = state.isLogin && state.user?.user_role === 1;
   const isUser = state.isLogin && state.user?.user_role === 4;
+
   if (state.loading || state.isLogin === null) {
     return <Loader />;
   }
 
-  if (!state.isLogin) {
-    return (
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/forgetpassword" element={<Forgetpassword />} />
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>
-    );
-  }
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/product" element={<Products />} />
+      <Route path="/blog" element={<Allblogs />} />
+      <Route path="/forgetpassword" element={<Forgetpassword />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/productsdetails/:id" element={<Productsdetail />} />
 
-  if (isUser) {
-    return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product" element={<Products />} />
-        <Route path="/blog" element={<Allblogs />} />
-        <Route path="/forgetpassword" element={<Forgetpassword />} />
-        <Route path="/Checkout" element={<Checkout />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/productsdetails/:id" element={<Productsdetail />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    );
-  }
+      {!state.isLogin && (
+        <>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </>
+      )}
 
-  if (isAdmin) {
-    return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/addproduct" element={<AddProducts />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/addcategories" element={<AddCategories />} />
-        <Route path="/product" element={<Products />} />
-        <Route path="/blog" element={<Allblogs />} />
-        <Route path="/productsdetails/:id" element={<Productsdetail />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    );
-  }
+      <Route
+        path="/checkout"
+        element={
+          state.isLogin ? <Checkout /> : <Navigate to="/login" replace />
+        }
+      />
 
-  return <Navigate to="/login" />;
+      {isAdmin && (
+        <>
+          <Route path="/addproduct" element={<AddProducts />} />
+          <Route path="/addcategories" element={<AddCategories />} />
+        </>
+      )}
+
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 };
 
 export default CustomRoutes;
