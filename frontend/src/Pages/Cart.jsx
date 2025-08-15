237b73f12de55c83a9e41bcf6d1193db8010b697
Taha Siraj <Pikachugaming899@gmail.com>
2025-08-15 12:40:34 -0700

    updated

--- frontend/src/Pages/Cart.jsx
+++ frontend/src/Pages/Cart.jsx
@@ -4,6 +4,7 @@ import { GlobalContext } from '../Context/Context';
 import api from '../Api';
 import { Toaster , toast } from 'react-hot-toast';
 import { IoMdCloseCircle } from "react-icons/io";
+import { Link } from 'react-router-dom';
 const Cart = ({ onClose }) => {
   const { state, dispatch } = useContext(GlobalContext);
   const [allCart, setAllCart] = useState([]);
@@ -41,8 +42,8 @@ const Cart = ({ onClose }) => {
     <div className="h-screen overflow-scroll font-poppins z-50 fixed top-0 right-0 w-full bg-black/40 flex justify-end" onClick={onClose}>
       <Toaster position='bottom-right' />
       <div onClick={(e) => e.stopPropagation()} className="h-full w-[400px] bg-white flex justify-between flex-col">
-        <div>
-          <div  className="flex justify-between p-4">
+        <div className='px-4'>
+          <div  className="flex justify-between pt-5">
             <h1 className="text-2xl font-bold">Shopping Cart</h1>
           <FaRegWindowClose
             className="text-2xl text-gray-500 cursor-pointer"
@@ -66,10 +67,12 @@ const Cart = ({ onClose }) => {
           
 
         </div>
-
-
         <div className='px-10'>
             <p className='text-md text-black font-bold'>Total: Rs: {subtotal} </p>
+        </div>
+        <div className='p-4 '>
+          <Link to="/cart" className='bg-white rounded-full border-2 capitalize border-black text-black no-underline py-2 px-4 '>cart</Link>
+          <Link to="/cart" className='bg-white rounded-full border-2 capitalize border-black text-black no-underline py-2 px-4 '>cart</Link>
         </div>
       </div>
     </div>
